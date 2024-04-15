cac4fcebee98963f9c7f8aadab34fc4dda078b03
maxDroll <mdroll6088.stu.d214.org>
2024-04-15 12:56:19 -0500

    Made a navigation Stack and a navigation link

--- ContentView.swift
+++ ContentView.swift
@@ -2,11 +2,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            NavigationLink("Click Me") {
+                NounView()
+            }
         }
     }
 }
